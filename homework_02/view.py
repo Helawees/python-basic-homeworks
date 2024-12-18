cffd6e36548e6a7ec47bf8f76abecc439fff91d2
Elena <96175542+Helawees@users.noreply.github.com>
2024-12-18 16:07:11 +0100

    changes in view after review
    Функции и классы отделяем двумя пустыми строками
    Необходимо документировать наш код
    Добавьте докстринги для классов и функций
    Тесты вынесите в отдельную директорию tests
    И фикстуры в тесте вынесите в conftest.py

--- homework_02/view.py
+++ homework_02/view.py
@@ -16,23 +16,30 @@ menu_txt = (f"Справочник OTUS 2024\n"
 action_menu_txt = (f"Для выхода из программы введите 'e'\n"
                    f"Для вывода меню введите 'm'\n")
 
+
 def print_incorrect_input():
     cprint("Некорректный ввод!\n", 'red')
+
 
 def print_added_success(contact_name):
     cprint(f"Контакт {contact_name} успешно добавлен!", 'green')
 
+
 def print_deleted_success():
     cprint(f"Контакт успешно удалён!", 'green')
+
 
 def print_edited_success(contact_name):
     cprint(f"Контакт {contact_name} успешно обновлён!", 'green')
 
+
 def print_smth_went_wrong():
     cprint("Что-то пошло не так, попробуйте еще раз!", 'red')
 
+
 def print_no_contact_data():
     cprint("Не предоставлены данные: имя или телефон контакта!", 'red')
+
 
 def get_new_contact_data():
     contact_name = input("Введите имя контакта: ")
@@ -43,17 +50,21 @@ def get_new_contact_data():
                     'comment': contact_comment}
     return contact_item
 
+
 def get_search_criteria():
     input_choice = input("Введите критерий поиска: ")
     return input_choice.lower()
+
 
 def print_menu():
     input_choice: str = input(menu_txt + "Ваш выбор: ")
     return input_choice.lower()
 
+
 def print_action_menu():
     input_choice: str = input(action_menu_txt)
     return input_choice.lower()
+
 
 def print_data(data):
     if data:
@@ -64,9 +75,11 @@ def print_data(data):
     else:
         cprint("Нет данных!\n", 'red')
 
+
 def edit_choice():
     choice = input("Введите id контакта, который желаете изменить: ")
     return choice.lower()
+
 
 def delete_choice():
     choice = input("Введите id контакта, который желаете удалить: ")
